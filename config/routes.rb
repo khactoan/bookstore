3b200e445e2007f164e839a23975954d10f1604b
ToanDK <khactoan96@gmail.com>
2017-12-14 15:59:50 +0700

    Finish user add item to cart and destroy with ajax

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => "/ckeditor"
   root "books#index"
   devise_for :users
+
   resources :books
+  resources :comments
+  resources :line_items
+  resources :carts
 
   namespace :admin do
     resources :books
   end
-
-  resources :comments
 end
